f213a964f3dafb7ed2a4a465c47cbc0a883da174
Jhaymar Sabino <jhaymarsabino@gmail.com>
2022-02-08 21:07:34 -0800

    Fixed bugs that stopped deployment using Netlify.

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,50 +1,103 @@
 import React, { useState, useEffect, createRef } from 'react';
-import { CircularProgress, Grid, Typography, InputLabel, Button, FormControl, Stack, Card, CardContent, CardHeader, Input } from '@material-ui/core';
+import {Grid, Typography } from '@material-ui/core';
+import Button from 'react-bootstrap/Button';
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import Card from 'react-bootstrap/Card'
+
 
 import useStyles from './styles';
-import { fontWeight } from '@mui/system';
+import { Co2Sharp } from '@mui/icons-material';
 
-const List = ({questions, difficulty, type, setUserAnswer}) => {
+function shuffle(array) {
+    let temp_arr = [];
+    let indices = [0, 1, 2, 3]
+    let randomIndex = 0;
+    let i = 0;
+
+    //console.log("Actual array: " + array);
+    while (indices.length != 0) {
+        randomIndex = (Math.floor(Math.random() * 100)) % indices.length;
+        temp_arr = temp_arr.concat(array[indices[randomIndex]]);
+        indices.splice(randomIndex, 1); //Remove the element
+        i++;
+    }
+
+    return temp_arr;
+}
+
+const List = ({questions, difficulty, setUserAnswer, userAnswer, questionPoints, choices, userScore, setUserScore}) => {
     const classes = useStyles();
-    const [choices, setChoices] = useState([]);
-    const answer = [];
-    console.log({questions});
+    const [choice_value, setChoiceValue] = useState('');
+    const [isAnswerClicked, setAnswerClicked] = useState(false);
+    let userChoice = '';
+    let correct_answer = '';
 
+    const confirmed = (correct_answer) => {
+        //isAnswerClicked = true;
+        setChoiceValue('');
+        setUserAnswer(choice_value);
+        console.log("The user answer: " + userAnswer + " the choice value: " + choice_value);
+        (decodeURIComponent(correct_answer) == decodeURIComponent(choice_value)) ? console.log("YOU ARE CORRECT") : console.log("YOU ARE WRONG");
+        console.log("Before adding: " + questionPoints + " User score: " + userScore);
+        (decodeURIComponent(correct_answer) == decodeURIComponent(choice_value)) ? setUserScore(Number(userScore) + Number(questionPoints)) :  setUserScore(Number(userScore) - Number(questionPoints));
+        console.log("The user score: " + userScore);
+    }
+
+    useEffect(() => {
+        userChoice = {choice_value};
+    }, [choice_value]);
+
+    const choose_variant = (correct_answer, choice_value) => {
+        if (isAnswerClicked) {
+            if ((decodeURIComponent(correct_answer) == decodeURIComponent(choice_value))) {
+                return "success";
+            } else {
+                return "danger";
+            }
+        } else {
+            return "outline-dark";
+        }
+    }
 
     return (
-        <Grid container style={{marginTop: 20, display: 'flex', justifyContent: 'center'}} >
-            <Grid item>
-                {questions?.map((question, i) => (
-                    setChoices(questions[i].incorrect_answers.concat(questions[i].correct_answer)),
-                    console.log("Answer: " + choices),
+        <Grid container style={{marginTop: 20, display: 'flex', justifyContent:'center', marginLeft: 30, width: '100%'}} >
+            {questions?.map((question, i) => (
+                <Grid item id="1">
+                    <Card key={question} style={{marginTop: 20, marginRight: '100px'}}> 
+                        <Card.Header> 
+                            <Typography variant="h5" style={{fontWeight: 'bold'}}> {decodeURIComponent(questions[i].category)}  
+                                <text style={{display: 'inline', justifyContent: 'right'}}> for {questionPoints} </text>
+                            </Typography>
+                        </Card.Header> 
+                        <Card.Text style={{margin: 20}}> 
+                            <Typography variant="subtitle1" style={{fontWeight: 'bold'}}> {decodeURIComponent(questions[i].question)}  </Typography>
+                        </Card.Text>
 
-                    
-                    <Card sx={{maxWidth: 345}} style={{marginTop: 20}}> 
-                        <CardHeader title={decodeURIComponent(questions[i].category)} />
-                        <CardContent> 
-                            <Typography variant="subtitle1"> {decodeURIComponent(questions[i].question)}  </Typography>
-                        </CardContent>
-                        <FormControl className={classes.formControl}>
-                            <InputLabel htmlFor="my-input">Answer</InputLabel>
-                            <Input id="answer" aria-describedby="my-helper-text" onChange={(answer) => setUserAnswer(answer)} />
-                        </FormControl>
-                        <Grid container spacing={3} style={{display: 'flex', justifyContent: 'center', marginBottom: 20}} >
-                            <Grid item > 
-                                <Button color="primary" variant="outlined" style={{borderColor: 'black', borderWidth: 2}}>Contained</Button>
+                        {(question.type == "multiple") ? (
+                            correct_answer = question.correct_answer,
+                            console.log("The correct answer : " + correct_answer),
+
+                            <Grid container spacing={3} style={{display: 'flex', justifyContent: 'center', marginBottom: 20, marginRight: 30}} >
+                                <Grid item id={i}>
+                                    <ToggleButton variant={choose_variant()} value={choices[0]} onClick={(e) => setChoiceValue(choices[0])} className={classes.button}> {decodeURIComponent(choices[0])} </ToggleButton>
+                                    <ToggleButton variant="outline-dark" value={choices[1]} onClick={(e) => setChoiceValue(choices[1])} className={classes.button}> {decodeURIComponent(choices[1])} </ToggleButton>
+                                    <ToggleButton variant="outline-dark" value={choices[2]} onClick={(e) => setChoiceValue(choices[2])} className={classes.button}> {decodeURIComponent(choices[2])} </ToggleButton>
+                                    <ToggleButton variant="outline-dark" value={choices[3]} onClick={(e) => setChoiceValue(choices[3])} className={classes.button}> {decodeURIComponent(choices[3])} </ToggleButton>
+                                </Grid>
                             </Grid>
-                            <Grid item> 
-                                <Button color="primary" variant="outlined" style={{borderColor: 'black', borderWidth: 2}}>Contained</Button>
-                            </Grid>
-                            <Grid item> 
-                                <Button color="primary" variant="outlined" style={{borderColor: 'black', borderWidth: 2}}>Contained</Button>
-                            </Grid>
-                            <Grid item> 
-                                <Button color="primary" variant="outlined" style={{borderColor: 'black', borderWidth: 2}}>Contained</Button>
-                            </Grid>
-                        </Grid>
-                    </Card> 
-                ))}
-            </Grid>
+                            ) : (
+                                <Grid container spacing={3} style={{display: 'flex', justifyContent: 'center', marginBottom: 20}} >
+                                    <Grid item id={i}>
+                                        <ToggleButton variant={choose_variant()} value={true} onClick={(e) => setChoiceValue(true)} className={classes.button}> True </ToggleButton>
+                                        <ToggleButton variant={choose_variant()} value={false} onClick={(e) => setChoiceValue(false)} className={classes.button}> False </ToggleButton>
+                                    </Grid>
+                                </Grid>
+                            )
+                        }
+                    </Card>,
+                    <Button variant="primary" disabled={(choice_value) == '' ? true: false } onClick={() => confirmed(correct_answer)} style={{marginTop: 10}}> Submit </Button>
+                </Grid>
+            ))}
         </Grid>
     );
 }
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React, {useState, useEffet} from 'react';
+
+const Login = () => {
+
+    return (
+        <div className="main"> Login Page </div>
+    );
+}
+
+export default Login;
